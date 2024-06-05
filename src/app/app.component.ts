47960e3da54bb1dce7c343c89bad69daed57d347
Gonzalo <cacalo219@hotmail.com>
2024-06-05 17:05:59 -0300

    Fin video 2 - Estética de consola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BotoneraComponent } from "./components/botonera/botonera.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, BotoneraComponent]
 })
 export class AppComponent {
   title = 'ta-te-ti-online-en-vivo';
